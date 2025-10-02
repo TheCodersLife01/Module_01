96c8fa294335b6a01118ae83536d0310201c1807
TheCodersLife01 <sftcoder@gmail.com>
2025-10-02 13:04:17 +0530

    Add files via upload

---
+++ Assignment 1/MathematicalOperations.py
@@ -0,0 +1,15 @@
+# Get input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform operations
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+# Display results
+print(f"Addition: {add}")
+print(f"Subtraction: {subtract}")
+print(f"Multiplication: {multiply}")
+print(f"Division: {divide}")
